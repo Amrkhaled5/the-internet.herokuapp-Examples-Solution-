1b767ca3b7afafdad085052308b5b5503395e6e1
amrr <amrr>
2025-08-06 23:46:30 +0300

    All Tasks Done

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -2,13 +2,19 @@ package base;
 
 import Pages.HomePage;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 import java.time.Duration;
 import java.util.List;
+
+import static java.io.File.*;
 
 public class BaseTest {
     protected HomePage homePage;
@@ -22,6 +28,20 @@ public class BaseTest {
         driver.get("https://the-internet.herokuapp.com/");
         homePage=new HomePage(driver);
     }
+
+//    @AfterMethod
+//    public void takeScreenshot(ITestResult result){
+//        if (ITestResult.FAILURE == result.getStatus()) {
+//            var camera = (TakesScreenshot) driver;
+//            File screenshot = camera.getScreenshotAs(OutputType.FILE);
+//            try {
+//                Files.move(screenshot.toPath(), new File("resources/screenshoot/" + result.getName() + ".png").toPath(), StandardCopyOption.REPLACE_EXISTING);
+//            } catch (IOException e) {
+//                throw new RuntimeException(e);
+//            }
+//        }
+//    }
+
 
     @AfterClass
     public void sutdown(){
